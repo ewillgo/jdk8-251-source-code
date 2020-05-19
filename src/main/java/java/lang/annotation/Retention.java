4f041720effac7b6b15794b25d942f0e26cc2420
liangwenwei <284115645@qq.com>
2020-05-19 10:29:35 +0800

    init

--- src/main/java/java/lang/annotation/Retention.java
+++ src/main/java/java/lang/annotation/Retention.java
@@ -31,10 +31,16 @@ package java.lang.annotation;
  * an annotation type declaration, the retention policy defaults to
  * {@code RetentionPolicy.CLASS}.
  *
+ * 指定注解的在VM中的保存策略，如果自定义注解没有出现@Retention，
+ * 则缺省的保存策略是：RetentionPolicy.CLASS
+ *
  * <p>A Retention meta-annotation has effect only if the
  * meta-annotated type is used directly for annotation.  It has no
  * effect if the meta-annotated type is used as a member type in
  * another annotation type.
+ *
+ * 如果要让这个元注解生效，则需要在自定义注解的最外层进行声明
+ * 如果把这个元注解声明到自定义注解的成员里，则不生效
  *
  * @author  Joshua Bloch
  * @since 1.5
--- src/main/java/java/lang/annotation/RetentionPolicy.java
+++ src/main/java/java/lang/annotation/RetentionPolicy.java
@@ -31,12 +31,18 @@ package java.lang.annotation;
  * in conjunction with the {@link Retention} meta-annotation type to specify
  * how long annotations are to be retained.
  *
+ * 注解保存策略
+ * 这些枚举常量描述了不同策略的保存细节
+ * 它们和@Retention一起使用来指定自定义注解的保存策略
+ *
  * @author  Joshua Bloch
  * @since 1.5
  */
 public enum RetentionPolicy {
     /**
      * Annotations are to be discarded by the compiler.
+     *
+     * 自定义注解会被编译器抛弃，只存在源码中
      */
     SOURCE,
 
@@ -44,12 +50,16 @@ public enum RetentionPolicy {
      * Annotations are to be recorded in the class file by the compiler
      * but need not be retained by the VM at run time.  This is the default
      * behavior.
+     *
+     * 自定义注解会被编译器编译进类文件，但不加载到虚拟机运行时里，这个是缺省的策略
      */
     CLASS,
 
     /**
      * Annotations are to be recorded in the class file by the compiler and
      * retained by the VM at run time, so they may be read reflectively.
+     *
+     * 自定义注解会被编译器编译到类文件里和被虚拟机加载到运行时，所以这些注解可以通过反射读取
      *
      * @see java.lang.reflect.AnnotatedElement
      */
